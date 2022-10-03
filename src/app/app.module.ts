101b90562b2cdff64eb80b8b3a4fa6f7015e9f67
Ashik Basheer <ashikdnd@gmail.com>
2022-10-03 22:42:05 +0530

    Added tailwind, angular material and stylized header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { PreferenceComponent } from './preference/preference.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {PreferenceComponent} from './preference/preference.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+
 
 @NgModule({
   declarations: [
@@ -16,9 +21,14 @@ import { PreferenceComponent } from './preference/preference.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
+import {Route, RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
 
-
+const routes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
 
 @NgModule({
   declarations: [
     BannerComponent,
-    FeaturedProductsComponent
+    FeaturedProductsComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
